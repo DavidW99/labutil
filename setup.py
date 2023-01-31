637bd7315fdba78c84235d4f99cb8339cd9abbcf
Linux-cpp-lisp <1473644+Linux-cpp-lisp@users.noreply.github.com>
2023-01-31 13:39:25 -0500

    Python packaging for easy install

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="labutil",
+    version="0.2.0",
+    author="Boris Kozinsky",
+    python_requires=">=3.7",
+    packages=find_packages(
+        include=["labutil", "labutil.*"]
+    ),
+    install_requires=["ase", "numpy"],
+    zip_safe=True,
+)
