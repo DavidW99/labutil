f56bc064938ada1c4cfbda8234cc84305aae5bc0
Linux-cpp-lisp <1473644+Linux-cpp-lisp@users.noreply.github.com>
2023-02-01 15:50:11 -0500

    clean up unused and print error messages

--- labutil/util.py
+++ labutil/util.py
@@ -1,52 +1,12 @@
-import os, subprocess, time
+import os, subprocess
 
 
 def run_command(command):
-    myrun = subprocess.Popen(command, shell=True)
-    pid = myrun.pid
-    #print("Running {} as {} ".format(command, myrun))
-    observe_job(process=myrun, justwait=True)
-    return pid
-
-
-def observe_job(process, justwait):
-    if justwait:
-        # process.wait()
-        os.waitpid(process.pid, 0)
-    else:
-        #print("Actively monitoring ", process, process.poll(), process.pid)
-        while check_pid(process.pid):
-            time.sleep(1)
-    #print("process {} is done".format(process))
-
-
-def check_pid(pid):
-    """ Check For the existence of a unix pid. Copied from stackoverflow."""
-    try:
-        os.kill(pid, 0)
-    except OSError:
-        return False
-    else:
-        return True
-
-
-def cleanup_dir(dirpath):
-    files = os.listdir(dirpath)
-    # add wildcard filters if needed
-    for file in files:
-        os.remove(os.path.join(dirpath, file))
-
-
-def read_file(fname):
-    with open(fname, "r") as fout:
-        text = fout.read()
-    return text
-
+    return subprocess.run(command, shell=True, capture_output=True, check=True,)
 
 def write_file(fname, text):
     with open(fname, 'w') as fin:
         fin.write(text)
-
 
 def prepare_dir(dirpath):
     if not os.path.exists(dirpath):
